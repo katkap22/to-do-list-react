4062d8671ed8d0af9ba45bfa102fc6fcb43f7816
EkaterinaKop <kat_kap@mail.ru>
2023-04-17 18:13:20 +0300

    add counter completed tasks and change status tas

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,34 +1,37 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 import Button from "../Button";
 
-function List({ tasks, removeTask, changeStatus }) { 
-
+function List({ tasks, removeTask, changeStatus }) {
   return (
-    <ul>
-      {
-        tasks.map(item => {
+    <>
+      <ul>
+        {tasks.map((item) => {
           const onRemoveTask = () => removeTask(item.id);
-          const onChangeStatusHandler = (event => changeStatus(item.id, event.currentTarget.checked));
-          
+          const onChangeStatusHandler = (event) =>
+            changeStatus(item.id, event.currentTarget.checked);
+
           return (
-            <li className={`item ${item.isDone ? 'is-done' : ''}`} 
-                key={item.id}>
+            <li
+              className={`item ${item.isDone ? "is-done" : ""}`}
+              key={item.id}
+            >
               <input
                 type="checkbox"
                 checked={item.isDone}
-                onChange={onChangeStatusHandler} />
+                onChange={onChangeStatusHandler}
+              />
               <span className="item_text">{item.title}</span>
               <Button
                 text="x"
                 onClickHandler={onRemoveTask}
-                customClass="item_btn" />
+                customClass="item_btn"
+              />
             </li>
           );
-            
-        })
-      }
-    </ul>
+        })}
+      </ul>
+    </>
   );
 }
 
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -15,6 +15,9 @@ class App extends React.Component {
       tasks: [], //массив объектов (id, title, isDone)
       filter: "all",
       onChange: false,
+      statusTask: "",
+      counter: 0,
+      counterMessage: "",
       error: null
     };
   }
@@ -35,6 +38,7 @@ class App extends React.Component {
       this.setState({
         tasks: newTasks,
         inputValue: "",
+        statusTask: `Вы добавили задачу: ${newTask.title}. ${new Date()}`,
       });
     } else {
         this.setState({
@@ -54,6 +58,7 @@ class App extends React.Component {
         let newTasks = [newTask, ...tasks];
 
         this.setState({
+          statusTask: `Вы добавили задачу: ${newTask.title}. ${new Date()}`,
           tasks: newTasks,
           inputValue: "",
         }); 
@@ -67,10 +72,21 @@ class App extends React.Component {
 
   removeTask = (id) => {
     const { tasks } = this.state;
+    // let countCompletedTasks = counter;
+
+    const removeTask = tasks.filter(item => item.id === id);
+    
     const filteredToDoList = tasks.filter((item) => item.id !== id);
     this.setState({
+      statusTask: `Вы удалили задачу: ${removeTask[0].title}. ${new Date()}`,
       tasks: filteredToDoList,
     });
+    // if (removeTask[0].isDone === true) {
+    //   this.setState({
+    //     counter: countCompletedTasks - 1
+    //   })
+    // }
+    
   };
 
   changeFilter = (value) => {
@@ -81,18 +97,54 @@ class App extends React.Component {
 
   changeStatus = (taskId, isDone) => {
     const { tasks } = this.state;
+    // let countCompletedTasks = counter;
     let task = tasks.find(t => t.id === taskId); 
+    
     if(task) {
       task.isDone = isDone;
+      if (isDone === true) {
+        
+        this.setState({
+          tasks: [...tasks],
+          // counter: countCompletedTasks + 1,
+          statusTask: `Вы завершили задачу: ${task.title}. ${new Date()}`,
+        });
+
+      } else {
+
+        this.setState({
+          tasks: [...tasks],
+          // counter: countCompletedTasks - 1,
+          statusTask: `Вы отменили завершение задачи: ${task.title}. ${new Date()}`,
+        });
+      }
     } 
+  }
+
+  counterCompletedTask = () => {
+    const { tasks } = this.state;
+
+    let count = 0;
+
+    tasks.map(task => {
+      if (task.isDone === true) {
+        count = ++count;
+      }
+      return count;
+    })
     this.setState({
-      tasks: [...tasks]
+      counter: count,
     });
-    }
+    this.setState({
+      counterMessage: `Количество выполненных задач из списка: ${count}`
+    })
+
+    setTimeout(() => this.setState({counterMessage: ""}), 5000);
+    
+  }
 
   render() {
-    const { inputValue, tasks, filter, error } = this.state;
-    
+    const { inputValue, tasks, filter, error, statusTask, counterMessage } = this.state;
     let tasksForTodolist = tasks;
 
     if (filter === "completed") {
@@ -115,13 +167,19 @@ class App extends React.Component {
           <Button text="add TODO" onClickHandler={this.addTask} />
         </div>
         {error && <div className="error-message">{error}</div>}
+        {statusTask && <div>{statusTask}</div>}
+        <Button
+          text="Показать количество выполненных задач"
+          onClickHandler={this.counterCompletedTask}
+          customClass="counter_btn"
+        ></Button>
+        <span>{counterMessage}</span>
         <List
           tasks={tasksForTodolist}
           removeTask={this.removeTask}
           changeStatus={this.changeStatus}
         />
-        <Filters changeFilter={this.changeFilter}
-                 filter={filter} />
+        <Filters changeFilter={this.changeFilter} filter={filter} />
       </div>
     );
   }
